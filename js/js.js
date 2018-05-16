2fd7bbc24bda0ef0825cd89bcc106cd1e1f6fb02
Joao Vitor Lima Nascimento <joaovitorjjlima@gmail.com>
2018-05-16 09:33:52 -0300

    Codigo migrado para o padrao Es6

--- js/js.js
+++ js/js.js
@@ -1,25 +1,24 @@
-var myObj;
+let myObj;
 
 
 requisitar = new XMLHttpRequest();
 requisitar.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         
-		txt = "";
-		myObj = JSON.parse(this.responseText);
+		let txt = '';
+		let myObj = JSON.parse(this.responseText);
 		
 		
-		txt += "<table border='1'> <tr> <th>Nome</th> <th>ID</th> <th>Nome do usuário</th> <th>Cidade</th> <th>Nome da empresa</th> </tr> "
+		 txt += '<table border="1"> <tr> <th>Nome</th> <th>ID</th> <th>Nome do usuário</th> <th>Cidade</th> <th>Nome da empresa</th> </tr>'
         
-		for (var x in myObj) {
-            txt += "<tr><td>" + myObj[x].name + "</td> <td>" + myObj[x].id + "</td><td>" + myObj[x].username + "</td><td>" + myObj[x].address.city + "</td> <td>" + myObj[x].company.name + "</td>  </tr>";
+		for (let x in myObj) {
+            txt += `<tr><td>${myObj[x].name}</td> <td>${myObj[x].id}</td><td>${myObj[x].username}</td><td>${myObj[x].address.city}</td> <td>${myObj[x].company.name}</td>  </tr>`;
         }
 
-        txt += "</table>"
-        document.getElementById("tabela").innerHTML = txt;
+         txt += '</table>'
+         document.getElementById('tabela').innerHTML = txt;
 			  
     }
 };
-requisitar.open("GET", 'https://jsonplaceholder.typicode.com/users', true);
+requisitar.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
 requisitar.send(); 
-
