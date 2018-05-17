7fe2855a078a53ceb8ecf82ab7c964502bae6fed
Joao Vitor Lima Nascimento <joaovitorjjlima@gmail.com>
2018-05-17 10:13:32 -0300

    Indentacao e finalizacao do exercicio

--- js/js.js
+++ js/js.js
@@ -1,48 +1,56 @@
-		function gerarTabela(){
-		let myObj;
+function gerarTabela(){
+	let myObj;
 
-		requisitar = new XMLHttpRequest();
-		requisitar.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
+	let requisitar = new XMLHttpRequest();
+	requisitar.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
 				
-				let txt = '';
-				let myObj = JSON.parse(this.responseText);
+			let txt = '';
+			myObj = JSON.parse(this.responseText);
 				
+				 txt += '<table border="1"> <tr> <th>Nome</th> <th>ID</th> <th>Nome do usuário</th> <th>Cidade</th> <th>Nome da empresa</th> </tr>';
 				
-				 txt += '<table border="1"> <tr> <th>Nome</th> <th>ID</th> <th>Nome do usuário</th> <th>Cidade</th> <th>Nome da empresa</th> </tr>'
-				
-				for (let x in myObj) {
-					txt += `<tr><td>${myObj[x].name}</td> <td>${myObj[x].id}</td><td>${myObj[x].username}</td><td>${myObj[x].address.city}</td> <td>${myObj[x].company.name}</td>  </tr>`;
-				}
+			for (let x in myObj) {
+				txt += `<tr><td>${myObj[x].name}</td> <td>${myObj[x].id}</td><td>${myObj[x].username}</td><td>${myObj[x].address.city}</td> <td>${myObj[x].company.name}</td>  </tr>`;
+			}
 
-				 txt += '</table>'
+				 txt += '</table>';
 				 document.getElementById('tabela').innerHTML = txt;
 					  
-			}
-		};
-		requisitar.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
-		requisitar.send();
 		}
+	};
+	requisitar.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+	requisitar.send();
+}
 
-		function alterarUsu(){
+function alterarUsu(){
 				
-			var myObj;
-			var idAlteracao = parseInt(document.getElementById("id").value);
+	var myObj;
+	var idAlteracao = parseInt(document.getElementById('id').value);
 				
-		requisitar = new XMLHttpRequest();
-		requisitar.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
+	let requisitar = new XMLHttpRequest();
+	requisitar.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
 					
-				myObj = JSON.parse(this.responseText);
+			myObj = JSON.parse(this.responseText);
 				
-				myObj.name = document.getElementById("nome").value;
-				myObj.username = document.getElementById("nomeUsu").value;
-				myObj.address.city = document.getElementById("cidade").value;
-				myObj.company.name = document.getElementById("nomeEmpresa").value;
-				
-				$('#alerta').show();
-				}
-			};
-			requisitar.open('PATCH', 'https://jsonplaceholder.typicode.com/users/'+idAlteracao, true);
-			requisitar.send(myObj);
+			myObj.name = document.getElementById('nome').value;
+			myObj.username = document.getElementById('nomeUsu').value;
+			myObj.address.city = document.getElementById('cidade').value;
+			myObj.company.name = document.getElementById('nomeEmpresa').value;
+			limparCampos();
+			$('#alerta').show();
 		}
+	};
+	requisitar.open('PATCH', 'https://jsonplaceholder.typicode.com/users/'+idAlteracao, true);
+	requisitar.send(myObj);
+}
+		
+function limparCampos()
+{
+	document.getElementById('nome').value = '';
+	document.getElementById('nomeUsu').value = '';
+	document.getElementById('cidade').value = '';
+	document.getElementById('nomeEmpresa').value = '';
+	document.getElementById('id').value = '';
+}
